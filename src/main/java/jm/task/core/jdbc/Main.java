a7259446b35e8f7cba086269beca3a24de74a32e
Konischev Dmitriy <72737936+Konischevd@users.noreply.github.com>
2021-01-03 21:54:42 +0300

    Java pre-project. Практическая задача 1.1.3 - finished!

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService u1 = new UserServiceImpl();
+
+        u1.createUsersTable();
+
+        u1.saveUser("Dimas", "Konischev", (byte) 27);
+        u1.saveUser("Ilya", "Trifon", (byte) 26);
+        u1.saveUser("Antonio", "Votin", (byte) 28);
+        u1.saveUser("Putin", "Vor", (byte) 99);
+
+        System.out.println(u1.getAllUsers());
+
+        u1.cleanUsersTable();
+
+        u1.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,24 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/kda_test?serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWORD = "170101";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName(DRIVER);
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Соединение с БД не установлено");
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
